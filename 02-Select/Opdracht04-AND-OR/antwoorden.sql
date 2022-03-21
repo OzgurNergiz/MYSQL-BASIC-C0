18db5f3a3cb2626af28df7c16ddb668ae7aae3dc
Özgür Nergiz <ozgur.nergiz1@student.rocva.nl>
2022-03-21 18:57:47 +0100

    upload

--- 02-Select/Opdracht04-AND-OR/antwoorden.sql
+++ 02-Select/Opdracht04-AND-OR/antwoorden.sql
@@ -1,30 +1,30 @@
 -- Opdracht 1 
-
+SELECT * FROM `players` WHERE nationality = 'spain' AND club = 'Chelsea'
 -- Opdracht 2 
-
+SELECT * FROM `players` WHERE nationality = 'spain' AND age = 17 AND wage = 15000
 -- Opdracht 3
-
+SELECT * FROM `players` WHERE club = 'Liverpool' AND age >= 20
 -- Opdracht 4
-
+SELECT * FROM `players` WHERE club = 'ajax' AND nationality = 'Netherlands'
 -- Opdracht 5
-
+SELECT * FROM `players` WHERE club = 'ajax' AND nationality <> 'Netherlands'
 -- Opdracht 6 
-
+SELECT name, age FROM `players` WHERE club = 'AZ alkmaar'
 -- Opdracht 7 
-
+SELECT name, age, club FROM `players` WHERE club = 'AZ alkmaar'
 -- Opdracht 8
-
+SELECT name, wage FROM `players` WHERE club = 'Manchester city' AND nationality = 'Brazil'
 -- Opdracht 9
-
+SELECT name FROM `players` WHERE age >= 30 AND wage <= 10000
 -- Opdracht 10
-
+SELECT name, age FROM `players` WHERE nationality = 'portugal' OR nationality = 'spain'
 -- Opdracht 11
-
+SELECT name, age FROM `players` WHERE nationality = 'portugal' OR club = 'Chelsea'
 -- Opdracht 12
-
+SELECT name, club FROM `players` WHERE age >= 40 AND wage >= 10000
 -- Opdracht 13
-
+SELECT * FROM `players` WHERE nationality = 'netherlands' AND club = 'ajax' OR club = 'FC utrecht'
 -- Opdracht 14
-
+SELECT * FROM `players` WHERE nationality = "England" AND age >= 20 AND wage = 100000
 -- Opdracht 15
-
+SELECT name, age, nationality FROM `players` WHERE nationality = "Argentina" OR nationality = "Brazil" AND age >= 25
--- 02-Select/Opdracht05-Functions-Alias/antwoorden.sql
+++ 02-Select/Opdracht05-Functions-Alias/antwoorden.sql
@@ -1,19 +1,21 @@
 -- Opdracht 1 
-
+SELECT MAX(wage) AS hoogste_loon_speler_fc_utrecht FROM `players` WHERE club = 'FC utrecht'
 -- Opdracht 2 
-
+SELECT AVG(wage) AS Avg_wage_all_players FROM `players` WHERE 1
 -- Opdracht 3
-
+SELECT AVG(wage) AS Avg_wage_FC_Groningen_players FROM `players` WHERE club = 'FC Groningen'
 -- Opdracht 4
-
+SELECT count(*) AS Total_Players_MC_MU FROM `players` WHERE club = 'Manchester City' OR club = 'Manchester United'
 -- Opdracht 5
-
+SELECT AVG(wage) AS Avg_wage_NL_players FROM `players` WHERE nationality = 'netherlands'
 -- Opdracht 6 
-
+SELECT AVG(wage) AS Avg_wage_players_under20 FROM `players` WHERE age <= 20
 -- Opdracht 7 
-
+SELECT AVG(wage) AS Avg_wage_players_above20 FROM `players` WHERE age >= 20
 -- Opdracht 8
-
+SELECT SUM(value) AS total_value_players_chelsea FROM `players` WHERE club = 'Chelsea'
 -- Opdracht 9
-
+SELECT AVG(age) AS avg_age_players FROM `players` WHERE 1
 -- Opdracht 10
+SELECT club, SUM(wage) AS total_income_players_Lp, AVG(value) AS avg_value_players_Lp FROM `players` WHERE club = 'Liverpool'
+
